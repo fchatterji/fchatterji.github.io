f7879eb3cd47946b25d01c9c960dc574e5b77c91
Florent <florent.chatterji@gmail.com>
2016-11-09 14:44:20 +0100

    complete redesign of the website
    redid the html part
    positionned the hero image and added a new image

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*
+This script adds a data-offset-top attribute to the navigation bar. 
+
+This attribute toggles the "affixing" of the navbar, ie the point at which it stays on top of the page.
+
+Here, it is set to the outer height (height + margin + padding) of the jumbotron, so that the navbar will 
+stay affixed when the user scrolls past the jumbotron.
+*/
+$("#navigation").affix({
+    offset: {
+        top: $("#jumbotron").outerHeight(true)
+    }
+});
